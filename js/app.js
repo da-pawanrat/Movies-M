d4801ea5f30fe67dace5602e96806b0c19e70d0b
atimay-p <61422582+atimay-p@users.noreply.github.com>
2020-10-18 19:35:43 +0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-document.addEventListener('prechange', function(event) {
-    document.querySelector('ons-toolbar .center')
+document.addEventListener('prechange', function (event) {
+  document.querySelector('ons-toolbar .center')
 
 
-  });
+});
 
-  $(function(){
-  var firebaseConfig = { 
+$(function () {
+  var firebaseConfig = {
     apiKey: "AIzaSyCCOpytQ9b-iPLC7IRtE7S4BK3AitxkdUk",
     authDomain: "plearnhub-509b2.firebaseapp.com",
     databaseURL: "https://plearnhub-509b2.firebaseio.com",
@@ -18,7 +18,7 @@ document.addEventListener('prechange', function(event) {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
-  
+
   var db = firebase.firestore();
   var index = 1;
   db.collection("trailer").get().then((querySnapshot) => {
@@ -33,9 +33,9 @@ document.addEventListener('prechange', function(event) {
 
       var vedio = `<video style='width:100%; height:100%' id='vedio_${id}' >
       <source src="${vedioUrl}" ></video>`
-      $("#"+id).append(vedio);
-      $("#vedio_"+id).hide();
-      hover(id,vedioUrl);
+      $("#" + id).append(vedio);
+      $("#vedio_" + id).hide();
+      hover(id, vedioUrl);
     })
   })
 
@@ -52,35 +52,55 @@ document.addEventListener('prechange', function(event) {
     })
   })
 
-
-  function hover(id){
-
-    $("#"+id).mouseover(function() {
-      $("#vedio_"+id).show();
-      $("#vedio_"+id).get(0).play();
+  var db = firebase.firestore();
+  var index = 1;
+  db.collection("trailer").limit(3).get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      var id = doc.id
+      var vedioUrl = doc.data().videoURL
+      var pic =
+        ` <ons-list-item style="grid-auto-rows: max-content;"> 
+          <ons-row>
+            <ons-col width="100px" style="margin-bottom: 10px;"><img src="${doc.data().posterURL}" style="width:100%; height: 100%;"></ons-col>
+              <ons-col>
+                <ons-row style="margin-left: 10px; margin-top: 10px ">${doc.data().title}</ons-row>
+                <ons-row style="margin-left: 10px; margin-top: 10px">${doc.data().shortstory}</ons-row>
+              </ons-col>
+          </ons-row>
+      `
+      $("#trailer").append(pic);
     })
-    $("#"+id).mouseleave(function() {
-      $("#vedio_"+id).hide();
-      $("#vedio_"+id).get(0).pause();
-      $("#vedio_"+id).get(0).currentTime = 0;
+  })
+
+  function hover(id) {
+
+    $("#" + id).mouseover(function () {
+      $("#vedio_" + id).show();
+      $("#vedio_" + id).get(0).play();
     })
-    
-    
+    $("#" + id).mouseleave(function () {
+      $("#vedio_" + id).hide();
+      $("#vedio_" + id).get(0).pause();
+      $("#vedio_" + id).get(0).currentTime = 0;
+    })
+
+
   }
 
 
   db.collection("movies").limit(5).get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        // console.log(`${doc.id} => ${doc.data()}`);
-        console.log(doc.data());
+      // console.log(`${doc.id} => ${doc.data()}`);
+      console.log(doc.data());
 
-        var row =     `<div class="card">
+      var row = `<div class="card">
         <img class="card-img-top" src="${doc.data().posterURL}" alt="">
         <div class="card-body">
           <h4 class="card-title">${doc.data().title}(${doc.data().years})</h4>
         </div>
       </div>`
-        $("#list").append(row);
+      $("#list").append(row);
     });
-  } );
   });
+});
